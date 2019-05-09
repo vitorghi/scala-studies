bf4fac8deb66f6a980f82c548d0e2eca8c35a0f2
Vitor <vitor.ghiraldelli@dextra-sw.com>
2019-05-09 08:55:54 -0300

    rational class

---
+++ src/main/scala/Rational.scala
@@ -0,0 +1,38 @@
+import scala.annotation.tailrec
+
+object rationals extends App {
+  val x = new Rational(1, 3)
+  val y = new Rational(5, 7)
+  val z = new Rational(3, 2)
+
+  println(x.sub(y).sub(z))
+}
+
+
+class Rational(x: Int, y: Int) {
+  require(y != 0, "denominator must not be zero")
+
+  def this(x: Int) = this(x, 1)
+
+  @tailrec
+  private def gcd(a: Int, b: Int): Int = if (b == 0) a else gcd(b, a % b)
+  private val divisor = gcd(x, y)
+
+  def numer = x / divisor
+  def denom = y / divisor
+
+  def less(that: Rational) = this.numer * that.denom < that.numer * this.denom
+
+  def max(that: Rational) = if (this.less(that)) that else this
+
+  def add(that: Rational) =
+    new Rational(
+      numer * that.denom + that.numer * denom,
+      denom * that.denom)
+
+  def neg: Rational = new Rational(-numer, denom)
+
+  def sub(that: Rational) = add(that.neg)
+
+  override def toString = numer + "/" + denom
+}
